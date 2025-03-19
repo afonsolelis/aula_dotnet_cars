c71ff99f364d77e038d42c3fb523ddd12b3cbbef
afonsolelis <afonsolelis@gmail.com>
2025-03-19 11:19:13 -0300

    aula legal

--- Volkswagen.Dashboard.Repository/CarModel.cs
+++ Volkswagen.Dashboard.Repository/CarModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Volkswagen.Dashboard.Repository.edu
+namespace Volkswagen.Dashboard.Repository
 {
     public class CarModel
     {
