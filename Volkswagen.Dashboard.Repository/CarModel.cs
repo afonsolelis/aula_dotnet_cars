6fecb1ffaddd444ec5f160f9a702c1db56d54cd3
afonsolelis <afonsolelis@gmail.com>
2025-03-19 11:17:47 -0300

    ola

--- Volkswagen.Dashboard.Repository/CarModel.cs
+++ Volkswagen.Dashboard.Repository/CarModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Volkswagen.Dashboard.Repository
+namespace Volkswagen.Dashboard.Repository.edu
 {
     public class CarModel
     {
